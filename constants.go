94f2a041b3ea39a394ac495b23046da2992b0f4e
Nick Verner <59479310+naevern@users.noreply.github.com>
2024-12-27 00:55:42 -0800

    moved to a new folder (#23)
    * moved to a new folder
    
    * refactor: consolidate API endpoint constants
    
    - Move API endpoint constants from opensea_client.go to constants.go
    - Remove duplicate endpoint declarations
    - Clean up spacing in constant declarations

--- constants.go
+++ constants.go
@@ -1,3 +1,7 @@
+package opensea
+
 const (
-	singleContractEndpoint = "/api/v1/asset_contract"
-) 
+	testnetAPI = "https://testnets-api.opensea.io"
+	contractEP = "/api/v1/asset_contract"
+	assetEP    = "/api/v1/asset"
+)
--- errors.go
+++ errors.go
@@ -1,2 +1,6 @@
+package opensea
+
+import "errors"
+
 // ErrEmptyContractAddress is returned when attempting to get a contract with an empty address
-var ErrEmptyContractAddress = errors.New("contract address cannot be empty") 
+var ErrEmptyContractAddress = errors.New("contract address cannot be empty")
